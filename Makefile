fca5561c6cc8465531a86f867b26d481058b3c4f
craco <gabriel_craco@hotmail.com>
2025-10-20 13:59:28 -0300

    first commit

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	gcc -c c8vm.c
+	gcc -c main.c
+	gcc -o chip8 c8vm.o main.o
+	
---
+++ c8vm.h
@@ -0,0 +1,19 @@
+#pragma once 
+
+#include <stdint.h>
+typedef struct VM {
+    uint8_t RAM[4096];      // Memória 4KB
+    uint16_t PC;            // Program Counter
+    uint8_t V[16];          // Registradores de propósito geral
+    uint8_t SP;             // Stack Pointer
+    uint16_t I;             // Registrador de índice
+    uint16_t stack[16];     // Pilha
+    uint8_t DISPLAY[64*32];// Tela
+} VM;
+
+void VM_Inicializar(VM* vm, uint16_t pc_inicial);
+void VM_CarregarROM(VM* vm, 
+                    char* arq_rom, 
+                    uint16_t pc_inicial);
+void VM_ExecutarInstrucao(VM* vm);
+void VM_ImprimirRegistradores(VM* vm);
